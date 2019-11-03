259faeaf3b4edb0000b8c5e2d4abe0e9b70f9a3f
megan9572 <megan9572@gmail.com>
2019-11-03 04:58:10 -0500

    add server

--- server/database/main.py
+++ server/database/main.py
@@ -1,39 +1,29 @@
-import tornado.ioloop
-import tornado.web
+from flask import Flask
 import json
 import sqlite3
 
+app = Flask(__name__)
 conn = sqlite3.connect('test.db')
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        conn.execute("CREATE TABLE IF NOT EXIST characters (CHAR_NAME TEXT, points TEXT")
+@app.route('/')
+def createTable():
+    conn.execute("CREATE TABLE IF NOT EXIST characters (CHAR_NAME TEXT, points TEXT")
 
-class NewCharHandler(tornado.web.RequestHandler):
-    def post(self, jsonString):
-        parse = json.loads(jsonString)
-        name = parse("name")
-        arrayPoints = json.dumps(parse("points"))
-        conn.execute("INSERT INTO characters VALUES (?,?)", (name, arrayPoints))
-
-class sendInputResponse(tornado.web.RequestHandler):
-    def get(self, jsonString):
-        ...
-
-    def post(self, jsonString):
-        self.write(jsonString)
-
-def make_app():
-    return tornado.web.Application({
-        (r"/", MainHandler),
-        (r"/saveChar", NewCharHandler),
-        (r"/help", sendInputResponse)
-    })
-
-if __name__ == "__main__":
-    app = make_app()
-    app.listen(8080)
-    tornado.ioloop.IOLoop.current().start()
+@app.route('/saveChar', methods = ['GET'])
+def saveChar(jsonString):
+    parse = json.loads(jsonString)
+    name = parse("name")
+    arrayPoints = json.dumps(parse("points"))
+    conn.execute("INSERT INTO characters VALUES (?,?)", (name, arrayPoints))
 
 conn.commit()
 conn.close()
+
+@app.route('/sendInput', methods = ['GET', 'POST'])
+def sendInputResponse(jsonString):
+    if request.method == 'GET':
+        return jsonString
+
+if __name__ == "__main__":
+    app.run()
+
