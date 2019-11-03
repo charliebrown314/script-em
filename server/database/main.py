73b52751a34521dea5a27d11f0ac26ed75c711fd
megan9572 <megan9572@gmail.com>
2019-11-03 06:15:09 -0500

    add server

--- server/database/main.py
+++ server/database/main.py
@@ -5,13 +5,14 @@ import sqlite3
 app = Flask(__name__)
 conn = sqlite3.connect('database.db')
 
-cursor = conn.cursor()
 @app.route('/')
 def table():
+    cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS characters (CHAR_NAME TEXT, points TEXT)")
 
 @app.route('/saveChar', methods = ['GET'])
 def saveChar(name, jsonString):
+    cursor = conn.cursor()
     cursor.execute("INSERT INTO characters VALUES (?,?)", (name, jsonString))
 
 conn.commit()
